eb9468bb6d34d05b11a4017dff8540b88a91e296
Xanthus <Xanthus58@protonmail.com>
2022-05-24 18:31:19 +0930

    added website

---
+++ js/commands.js
@@ -0,0 +1,101 @@
+var twitter = "https://twitter.com/Xanthus5862";
+var password = "XansCool";
+var instagram = "https://www.instagram.com/xanthus58/";
+var github = "https://github.com/Xanthus58";
+var email = 'mailto:xanthus58@protonmail.com';
+var VanillaRenewed = "https://github.com/Xanthus58/Vanilla-Renewed/tree/Experimental";
+var NetworkPoolCalculatorReforged = "https://github.com/Xanthus58/NetworkPoolCalculatorReforged";
+var StillAliveReforged = "https://github.com/Xanthus58/StillAliveReforged";
+var ValorantRandomizer = "https://github.com/Xanthus58/Valorant-Randomizer";
+var malacyte = "https://discordbotlist.com/bots/malacyte";
+var CCTweakedScripts = "https://github.com/Xanthus58/CC-Tweaked-Scripts";
+var CLIAdventureGame = "https://github.com/Xanthus58/CLI-Adventure-Game";
+var RockPaperScissorsCLI = "https://github.com/Xanthus58/Rock-Paper-Scissors-CLI";
+var AffirmationRequester = "https://github.com/Xanthus58/Affirmation-Requester";
+
+whois = [
+  "<br>",
+  "Hi, there! I am Xanthus👋",
+  ".",
+  "<span class=\"command\">-About Me-</span><span class=\"color2\">.</span>",
+  "I'm a Computer Enthusiast",
+  "Currently self teaching with online resources",
+  "Currently learning and growing my skillset in desktop development.",
+  "Server operator and maintainer in my spare time.",
+  "I like to expand my knowledge and explore new technologies.",
+  "In my free time, I like to listen songs and game.",
+  ".",
+  "<span class=\"command\">-Tech Stack-</span><span class=\"color2\">.</span>",
+  "Technologies that I've been learning and picked up along my programming journey.",
+  ".",
+  "<span class=\"command\">-Languages-</span><span class=\"color2\">.</span>",
+  "> Rust",
+  "> Python",
+  "> Lua",
+  ".",
+  "<span class=\"command\">-OS & IDE & Tools-</span><span class=\"color2\">.</span>",
+  "OS: Windows",
+  "IDE: Vscode",
+  ".",
+  "> GitHub",
+  "> Docker",
+  "> Git",
+  "> Markdown",
+  "<br>"
+];
+
+
+social = [
+  "<br>",
+  "<span class=\"command\">-Feel free to contact me on any socials below-</span><span class=\"color2\">.</span>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter.com/Xanthus5862' + '</a>',
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/xanthus58' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/Xanthus58' + "</a>",
+  'email         <a href="' + email + '" target="_blank">xanthus58@protonmail.com' + "</a>",
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "<span class=\"command\">Type the name of any project to navigate there</span><span class=\"color2\">.</span>",
+  "<span class=\"command\">-Utility-</span><span class=\"color2\">.</span>",
+  "> Malacyte | A multipurpose discord bot with 100+ features",
+  "> NetworkPoolCalculatorReforged | A crypto mining profitibilty CLI tool",
+  ".",
+  "<span class=\"command\">-Games-</span><span class=\"color2\">.</span>",
+  "> VanillaRenewed | A minecraft vanilla+ texture pack",
+  "> StillAliveReforged | A recreation of the 'Still Alive' song from portal",
+  "> ValorantRandomizer | A small CLI randomizer for the game 'Valorant'",
+  "> CCTweakedScripts | A collection of lua scripts for use in the minecraft mod 'Computer Craft'",
+  "> CLIAdventureGame | A very small terminal escape the room game",
+  "> RockPaperScissorsCLI | A small rock paper scissors game for the terminal",
+  ".",
+  "<span class=\"command\">-Miscellaneous-</span><span class=\"color2\">.</span>",
+  "> AffirmationRequester | Small API handeler that can generate motivation quotes. Has ai intergration",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Xanthus?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           Displays the list you are currently viewing',
+  '<span class="command">email</span>          Email me',
+  '<span class="command">clear</span>          Clears the terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+"  #     #                                        ",
+"  #   #    ##   #    # ##### #    # #    #  #### ",
+"   # #    #  #  ##   #   #   #    # #    # #     ",
+"    #    #    # # #  #   #   ###### #    #  #### ",
+"   # #   ###### #  # #   #   #    # #    #      #",
+"  #   #  #    # #   ##   #   #    # #    # #    #",
+" #     # #    # #    #   #   #    #  ####   #### ",
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">help</span><span class=\"color2\">.</span>",
+];
